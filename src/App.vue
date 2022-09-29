0b3f4b9cb4c01f5805394b2495943242e2abac86
Chanias <85565864+Chanias@users.noreply.github.com>
2022-09-29 16:50:25 +0200

    Creation des template + début code sur MoviesList

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <HeaderNav />
+
+    <div v-if="$route.path == '/'">
+      <!--si la route est / (racine du site) -->
+      <h1 class="pt-5 font-weight-light">Vos films préférés sont sur Top Movies !</h1>
+      <MoviesList :movies="movies" :loading="loading" :errored="errored" />
+    </div>
+
+    <div v-else>
+      <!--si la route est différente de / -->
+      <router-view :key="$route.fullPath"></router-view>
+    </div>
+
+    <FooterApp />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from './components/template/HeaderNav.vue'
+import FooterApp from './components/template/FooterApp.vue'
+import MoviesList from './components/utils/MoviesList.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderNav,
+    FooterApp,
+    MoviesList
+  },
+  data() {
+    return {
+      movies: null,
+      errored: false,
+      loading: false
+    }
+  },
+  created(){
+    console.log("created");
+    axios
+      .get('https://api.themoviedb.org/3/discover/movie?api_key=06a9b62fd61f37d8abcc00ee0a9a008f&language=FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+      .then(res => {
+        this.movies = res.data.results
+        console.log(this.movies)
+      })
   }
+ 
 }
+
 </script>
 
 <style>
@@ -21,6 +58,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/components/utils/MoviesList.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="container">
+    <h1>Les films</h1>
+</div>
+
+</template>
+
+<script>
+    import MoviesList from './components/utils/MoviesList.vue'
+    import axios from 'axios'
+
+export default {
+    name: "MoviesList",
+    components: {
+        MoviesList
+    },
+    props: [
+        "title",
+        "poster_path",
+        "release_date",
+        "vote_average",
+        "overwiew"
+    ],
+}
+
+
+</script>
